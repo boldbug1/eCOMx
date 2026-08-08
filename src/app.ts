c95cd682305c951cc996dafea8f78249d01d5ff9
boldbug1 <boldbug9@gmail.com>
2026-08-08 14:56:52 +0530

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,25 @@ import orderRouter from './routes/orders.js';
 import {logger} from './middleware/logger.js';
 import authRouter from './routes/authRoutes.js';
 import productsRouter from './routes/products.js';
+import cors,{CorsOptions} from 'cors'
 
 export const app:Application = express();
 
+const whiteList = ['http://localhost:3000']
+
+const corsOptions:CorsOptions={
+    origin:function(origin,callback){
+        if(!origin||whiteList.indexOf(origin) !== -1){
+            callback(null,true);
+        }else{
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus:200
+}
+
 //middleware
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(logger);
 app.use('/api/v1',orderRouter);
